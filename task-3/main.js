3e3c5124b7faf3f010f4447f71578a7eadb46397
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-15 17:42:44 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,15 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+      
+    let input=result.input;
+    let n=input.split(',');
+    let a=parseInt(n[0]);
+    let b=parseInt(n[1]); 
+    let c=parseInt(n[2]); 
+    if (a+b>c && b+a>c && c+b>a && c+a>a) {
+        console.log('YES');
+    }else if (a+b<=c || b+a<=c || c+b<=a || c+a<=a) 
+    {console.log('NO');}
   
 });
