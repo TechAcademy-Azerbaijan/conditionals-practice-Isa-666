2b13662f96c68ee58d1510eee997f59010232cb4
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-15 18:52:49 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -4,6 +4,16 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let n=result.input; //3456
+    let a=parseInt(n/1000); //3
+    let b=parseInt((n/100)%10); //4
+   let c=parseInt((n/10)%10); //5
+ let d=parseInt(n%10); //6
+
+ if (a%2==0 && b%2==0 && c%2==0 && d%2==0) {console.log('YES');
+    
+ }else if (a%2!==0 || b%2!==0 || c%2!==0 || d%2!==0) {console.log('NO');
+    
+}
   
 });
