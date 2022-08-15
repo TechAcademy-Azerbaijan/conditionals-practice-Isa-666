b16a61370de8b8f7f444d2d838e30de8fbb514ed
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-16 02:03:07 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -4,6 +4,16 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let n=result.input; //2322
+   let a=parseInt(n/1000); //2
+   let b=parseInt((n/100)%10); //3
+  let c=parseInt(n/10)%10; //2
+let d=parseInt(n%10); //2
+
+if (a+b>=c+d) {
+  console.log('YES');
+}else if (a+d<=c+b) {
+  console.log('NO');
+}
   
 });
