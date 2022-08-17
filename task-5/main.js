a00285053e9c8fedb4d4616b022ee6ad941537b6
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-17 14:46:25 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -4,6 +4,13 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+  let input=result.input.split(""); //1378
+    
+ for (let i = 0; i < input.length; i++) {
+ if (parseInt(input[i])===3 && parseInt(input[i+1])===7) {
+  console.log('YES');
+ }else{console.log('NO');}
+
+ }
   
 });
