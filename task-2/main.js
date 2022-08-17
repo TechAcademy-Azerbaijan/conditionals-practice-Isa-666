22c8dfab89d5de76af205dc9ec1bf4e6d8ae6dce
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-17 14:29:55 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -4,16 +4,19 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-       let input=result.input;
-    let n=input.split(',');
-    let x=parseInt(n[0]); //7
-    let a=parseInt(n[1]); //2
-    let b=parseInt(n[2]); //9
-    
-    if (x>=a && x<=b) {
+
+    let input = result.input;
+    let n = input.split(',');
+    let x = parseInt(n[0]); //7
+    let a = parseInt(n[1]); //2
+    let b = parseInt(n[2]); //7
+
+    if (x > a && x <= b) {
         console.log('IN');
-    }else if (x<=a || x>=b || x<=b) {
+    } else {
         console.log('OUT');
     }
-  
+
+    
+   
 });
